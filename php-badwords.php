207b948dca3745ead6c08008d9785de7f230c66e
PaoloCardinale09 <alfiozeronove@gmail.com>
2023-03-09 18:05:35 +0100

    final commit

---
+++ php-badwords.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+    <h2>Questo è il paragrafo che hai inserito</h2>
+    <p>
+        <?php echo $_GET["paragraph"]; ?>
+    </p>
+    <h3>Ha una lunghezza di </h3>
+    <p>  <?php echo strlen($_GET["paragraph"]);?> caratteri </p>
+</div>
+
+<div>
+    <h2>Questo è il tuo paragrafo dopo la censura</h2>
+    <?php echo str_replace($_GET["badword"], "***",$_GET["paragraph"]);
+    $censurato = str_replace($_GET["badword"], "***",$_GET["paragraph"]);
+    ?>
+    <h3>Ha una lunghezza di </h3>
+    <p>  <?php echo strlen($censurato) ;?> caratteri </p>
+</div>
+
+  
+</body>
+</html>
